dd4e6a548583f51b3f454e40016f752125025c1b
meliheng <melihcanyildiz9@gmail.com>
2021-12-24 15:44:45 +0300

    send email

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 const app = express();
 
+// Connect DB
+mongoose
+	.connect('mongodb://localhost/smartedu-db')
+	.then(() => {
+		console.log('Db connected successfuly');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 //Template Engine
 app.set('view engine', 'ejs');
 
+//global variable
+global.userIN = null;
+
 //Middleware
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static('public'));
+app.use(
+	session({
+		secret: 'my_keyboard_cat',
+		resave: false,
+		saveUninitialized: true,
+		store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+	})
+);
 
 //Routes
-app.get('/', (req, res) => {
-	res.status(200).render('index', {
-		page_name: 'index',
-	});
+app.use('*', (req, res, next) => {
+	userIN = req.session.userID;
+	next();
 });
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 const port = 3000;
 app.listen(port, () => {
 	console.log(`App started on port ${port}`);
---
+++ routes/userRoute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
+const router = express.Router();
+
+router.route('/signup').post(authController.createUser); // localhost/users/signup
+router.route('/login').post(authController.loginUser); // localhost/users/login
+router.route('/logout').get(authController.logoutUser); // localhost/users/logout
+router.route('/dashboard').get(authMiddleware, authController.getDashboardPage); // localhost/users/dashboard
+module.exports = router;
